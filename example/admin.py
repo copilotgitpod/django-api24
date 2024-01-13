22654905cb993b5bca11400d3f647497127520e2
copilotgitpod <155841626+copilotgitpod@users.noreply.github.com>
2024-01-13 16:07:46 +0400

    Update admin.py

--- example/admin.py
+++ example/admin.py
@@ -1,6 +1,3 @@
-from django.apps import AppConfig
+from django.contrib import admin
 
-
-class ExampleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'example'
+# Register your models here.
